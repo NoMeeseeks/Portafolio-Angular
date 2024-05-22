8ff14d46a9e8031ef8007bec5d7e4c65a7f4ab24
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-21 21:11:21 -0500

    Creacion de portafolio

---
+++ src/app/home/app.home.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: 'app.home.html',
+})
+export class AppHomeComponent {
+
+}

4123013a6391bad980e7e0a13c7dfaa3b37620d8
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-05-24 22:39:37 -0500

    Maquetacion de mi portafolio aun faltan ajuste de diseño

--- src/app/home/app.home.component.ts
+++ src/app/home/app.home.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { RepositorioComponent } from "../components/repositorio/repositorio.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RepositorioComponent],
   templateUrl: 'app.home.component.html',
 })
 export class AppHomeComponent {
 
+  items = [1, 2, 3, 4, 5]
 }
